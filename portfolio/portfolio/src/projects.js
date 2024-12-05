da0cca81bed9c9849198d363a8abbecdb87681ad
younguk <ifyounguk@gmail.com>
2024-12-05 11:59:47 +0900

    [FIX] project 필터링 작업 (리팩토링)

--- portfolio/portfolio/src/projects.js
+++ portfolio/portfolio/src/projects.js
@@ -11,18 +11,38 @@ const category = 0;
 const categories = document.querySelector(".categories");
 // querySelectorAll: 선택자에 해당하는 모든 요소들을 배열 형태로 return
 const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
 
 categories.addEventListener("click", (event) => {
   const filter = event.target.dataset.filter;
   if (filter == null) {
     return;
   }
+  // Active 매뉴를 재설정
+  handleActiveSelection(event.target);
+
+  // 프로젝트 필터링
+  filterProjects(filter);
+});
+
+// Active 매뉴를 재설정
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+// 프로젝트 필터링
+function filterProjects(filter) {
+  projectsContainer.classList.add("anim-out");
   projects.forEach((project) => {
-    console.log(project.dataset.type);
     if (filter === "all" || filter === project.dataset.type) {
-      project.style.display = "block"; // 기본값
+      project.style.display = "block"; // 기본값(보이게)
     } else {
       project.style.display = "none"; // 안보이게
     }
   });
-});
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}
